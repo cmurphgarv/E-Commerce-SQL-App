ad21345a99fbfa6e242c61121ee80c5e383145b4
Charlotte Murphey <murphey.charlotte@gmail.com>
2022-11-28 10:41:12 -0500

    added model column definitions

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,14 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await LibraryCard.findAll({
+      include: [{ model: Reader }],
+    });
+    res.status(200).json(libraryCardData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {

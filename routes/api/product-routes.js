d3094304459ddf5181199e982718fddc463b70b1
Charlotte Murphey <murphey.charlotte@gmail.com>
2022-11-30 13:31:38 -0500

    updated routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -75,7 +75,7 @@ router.put('/:id', (req, res) => {
   })
     .then((product) => {
       // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
+      return ProductTag.findAll({ where: { product_id: product.id } });
     })
     .then((productTags) => {
       // get list of current tag_ids
